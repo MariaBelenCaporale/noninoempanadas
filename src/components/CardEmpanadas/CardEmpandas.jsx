24a1d9199d499d50063c55da90d0bcbb57d7841e
belen <caporalebelen14@gmail.com>
2025-09-16 14:04:32 -0300

    cards

--- src/components/CardEmpanadas/CardEmpandas.jsx
+++ src/components/CardEmpanadas/CardEmpandas.jsx
@@ -1,18 +1,26 @@
 import './styles.css';
 import '../Button/Button';
+import { ArrowUpRight } from 'lucide-react';
 
 
-const CardEmpanadas = ({ empanada, nombre, click }) => {
+const CardEmpanadas = ({ empanada, nombre, click, descripcion }) => {
     return (
         <div className='contenedorEmpanada'>
             <div className='imgEmpanadas' onClick={click}>
                 <img className='sabor' src={empanada} />
             </div>
+            
+            <div className='datosEmpanada'>
             <div className='infoEmpanada'>
                 <h2 className='nombreEmpanada'>
                     {nombre}
                 </h2>
+                <p className='descripcionEmpanada'>{descripcion}</p>
             </div>
+                <div className='flechaCard'>
+                    <ArrowUpRight />
+                </div>
+                </div>
         </div>
     )
 };
