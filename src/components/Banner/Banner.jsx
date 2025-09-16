f7a02d9d23b53aeccb264d8dcc88112cf8451732
belen <caporalebelen14@gmail.com>
2025-09-16 14:05:41 -0300

    banner

--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -14,21 +14,21 @@ const Banner = () => {
                 <div className='pasosImgTextos'>
                     <img className='imgPasos' src={PasoUno} />
                     <div className='textosOrdenBanner'>
-                        <p className='tituloPasos'>Pedí en segundos</p>
+                        <p className='tituloPasosBanner'>Pedí en segundos</p>
                         <p className='textosBannerPasos'>Elegí tus empanadas favoritas y encargalas sin complicaciones.</p>
                     </div>
                 </div>
                 <div className='pasosImgTextos'>
                     <img className='imgPasos' src={PasoDos} />
                     <div className='textosOrdenBanner'>
-                        <p className='tituloPasos'>Llegamos cuando decimos</p>
+                        <p className='tituloPasosBanner'>Llegamos cuando decimos</p>
                         <p className='textosBannerPasos'>Recibí tu pedido a tiempo, calentito y listo para disfrutar.</p>
                     </div>
                 </div>
                 <div className='pasosImgTextos'>
                     <img className='imgPasos' src={PasoTres} />
                     <div className='textosOrdenBanner'>
-                        <p className='tituloPasos'>Empanadas que se disfrutan</p>
+                        <p className='tituloPasosBanner'>Empanadas que se disfrutan</p>
                         <p className='textosBannerPasos'>Recetas caseras, ingredientes frescos y el sabor de siempre.</p>
                     </div>
                 </div>
